68fe0ba169e2b0c2a6bc7b1bcfe410682ed42e68
AdityaManojkumar <adityambidar@gmail.com>
2025-06-18 22:38:47 +0530

    Test CI/CD pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
+import React from "react";
 
 export default [
   js.configs.recommended,
